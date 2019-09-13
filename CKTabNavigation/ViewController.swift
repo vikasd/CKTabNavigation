7f9c5867d95a06e9c0f135d7456f25e7b1a65be7
Vikas Dalvi <vikas.dalvi@hurix.com>
2019-09-13 13:03:41 +0530

    Initial Commit

---
+++ CKTabNavigation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CKTabNavigation
+//
+//  Created by Vikas Dalvi  on 13/09/19.
+//  Copyright © 2019 Vikas Dalvi . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CKTabNavigationTests/CKTabNavigationTests.swift
@@ -0,0 +1,34 @@
+//
+//  CKTabNavigationTests.swift
+//  CKTabNavigationTests
+//
+//  Created by Vikas Dalvi  on 13/09/19.
+//  Copyright © 2019 Vikas Dalvi . All rights reserved.
+//
+
+import XCTest
+@testable import CKTabNavigation
+
+class CKTabNavigationTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
